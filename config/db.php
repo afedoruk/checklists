8b97899ddedf93b6c1e224cd3d90ab6a7c55503f
Anna Fedoruk <afedoruk@debmorgan.debmorgan>
2014-05-22 19:52:35 +0400

    Added user registration

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=checklists',
+    'username' => 'checker',
+    'password' => 'checkmate',
     'charset' => 'utf8',
 ];
---
+++ models/RegisterForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use app\models\User;
+
+class RegisterForm extends Model
+{
+    public $username;
+	public $email;
+    public $password;
+	public $password_repeat;
+    
+	public function attributeLabels()
+    {
+        return [
+            'username' => 'Your name',
+            'password' => 'Your password',
+            'password_repeat' => 'Confirm password',
+        ];
+    }
+
+    private $_user = false;
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            // username and password are both required
+            [['username', 'email', 'password', 'password_repeat'], 'required'],
+            ['email', 'email'],
+            ['email', 'unique', 'targetClass'=>'\app\models\User'],
+            // password is validated by validatePassword()
+            ['password', 'compare'],
+        ];
+    }
+
+
+
+    public function register()
+    {
+        if ($this->validate()) {
+        	$user = new User();
+			$user->username=$this->username;
+			$user->email=$this->email;
+			$user->password=$this->password;
+			$user->save(); 
+            return $user->save();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     *
+    public function getUser()
+    {
+        if ($this->_user === false) {
+            $this->_user = User::findByUsername($this->username);
+        }
+
+        return $this->_user;
+    }*/
+}
